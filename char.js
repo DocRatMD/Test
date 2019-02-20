844731603a1c449ef2e0c599eafc8216b69249b6
DocRatMD <47792236+DocRatMD@users.noreply.github.com>
2019-02-20 01:42:37 -0600

    Add files via upload

--- char.js
+++ char.js
@@ -3,42 +3,40 @@ function Char(x,y) {
   this.y = y;
 
   this.show = function() {
-    image(dir,x,y,104,176);
+    //image(dir,x,y,104,176);
+    if (isStopped === true) {
+      image(dir,x,y,104,176);
+    }
   }
 
   this.move = function() {
+
+
     if (keyIsDown(65) && 0 <= x - 30) {
     x -= 3;
     //dir = loadImage('assets/leftchar.png');
     animation(leftwalk,x,y);
-    if (keyIsDown(16)) {
-      x -= 6;
-    }
+    isStopped = false;
   }
 
     if (keyIsDown(68) && width >= x + 30) {
     x += 3;
     //dir = loadImage('assets/rightchar.png');
     animation(rightwalk,x,y);
-  if (keyIsDown(16)) {
-    x += 6;
-  }
+    isStopped = false;
 }
 
 if (keyIsDown(87) && 0 <= y - 55) {
   y -= 3;
-  dir = loadImage('assets/upchar.png');
-  if (keyIsDown(16)) {
-    y -= 6;
-  }
+  //dir = loadImage('assets/upchar.png');
+  animation(upwalk,x,y);
+  isStopped = false;
 }
 
 if (keyIsDown(83) && height >= y + 50) {
-  y += 3;
-  dir = loadImage('assets/downchar.png');
-  if (keyIsDown(16)) {
-y += 6;
-  }
-}
+    y += 3;
+    animation(downwalk,x,y);
+    isStopped = false;
+    }
   }
 }
--- sketch.js
+++ sketch.js
@@ -2,19 +2,23 @@ let leftChar,rightChar,upchar,downchar;
 let char;
 let dir;
 let leftwalk,rightwalk,upwalk,downwalk;
+let isStopped;
 
 function preload() {
   leftChar = loadImage('assets/leftchar.png');
   rightChar = loadImage('assets/rightchar.png');
   upchar = loadImage('assets/upchar.png');
   downchar = loadImage('assets/downchar.png');
-  dir = loadImage('assets/rightchar.png');
-  leftwalk = loadAnimation('assets/leftwalk/leftwalk0001.png','assets/leftwalk/leftwalk0002.png');
-  rightwalk = loadAnimation('assets/rightwalk/rightwalk0001.png','assets/rightwalk/rightwalk0002.png');
+  dir = loadImage('assets/downchar.png');
+  leftwalk = loadAnimation('assets/leftwalk/leftwalk0001.png','assets/leftwalk/leftwalk0004.png');
+  rightwalk = loadAnimation('assets/rightwalk/rightwalk0001.png','assets/rightwalk/rightwalk0004.png');
+  upwalk = loadAnimation('assets/upwalk/upwalk0001.png','assets/upwalk/upwalk0004.png');
+  downwalk = loadAnimation('assets/downwalk/downwalk0001.png','assets/downwalk/downwalk0004.png');
 }
 
 function setup() {
 createCanvas(1200,800);
+isStopped = true;
 imageMode(CENTER);
 char = new Char(width/2,height/2);
 }
@@ -25,4 +29,9 @@ function draw() {
   char.move();
   //animation(leftwalk, width/2,height/2);
   //animation(leftwalk,100,100);
+
+}
+
+function keyReleased() {
+  isStopped = true;
 }
